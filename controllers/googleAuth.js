bb6cea4a967606b33ad4cbca91d8666109da8308
stmarys-jsoc-dallas <48659260+stmarys-jsoc-dallas@users.noreply.github.com>
2019-03-26 00:24:10 -0500

    Update googleAuth.js

--- controllers/googleAuth.js
+++ controllers/googleAuth.js
@@ -125,7 +125,7 @@ function listFiles() {
         gapi.client.drive.files.list({
           'pageSize': 10,
           'mimeType': 'application/vnd.google-apps.folder',
-          'fields': "nextPageToken, files(id, name,mimeType)"
+          'fields': "nextPageToken, files(id, name,mimeType,downloadUrl)"
         }).then(function(response) {
           console.log('Files:');
           var files = response.result.files;
@@ -141,3 +141,20 @@ function listFiles() {
         });
       }
 
+function downloadFile(file, callback) {
+  if (file.downloadUrl) {
+    var accessToken = gapi.auth.getToken().access_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', file.downloadUrl);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.onload = function() {
+      callback(xhr.responseText);
+    };
+    xhr.onerror = function() {
+      callback(null);
+    };
+    xhr.send();
+  } else {
+    callback(null);
+  }
+}
