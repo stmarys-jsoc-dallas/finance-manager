79f299dda7adae026f13a0ec7aea830472411c0f
stmarys-jsoc-dallas <48659260+stmarys-jsoc-dallas@users.noreply.github.com>
2019-03-25 21:24:45 -0500

    Update googleAuth.js

--- controllers/googleAuth.js
+++ controllers/googleAuth.js
@@ -126,15 +126,15 @@ function listFiles() {
           'pageSize': 10,
           'fields': "nextPageToken, files(id, name)"
         }).then(function(response) {
-          appendPre('Files:');
+          console.log('Files:');
           var files = response.result.files;
           if (files && files.length > 0) {
             for (var i = 0; i < files.length; i++) {
               var file = files[i];
-              appendPre(file.name + ' (' + file.id + ')');
+              console.log(file.name + ' (' + file.id + ')');
             }
           } else {
-            appendPre('No files found.');
+            console.log('No files found.');
           }
         });
       }
