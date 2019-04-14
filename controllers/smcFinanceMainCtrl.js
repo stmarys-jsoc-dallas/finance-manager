761d30f11f352015b1da309183406aa7bf1a47d0
stmarys-jsoc-dallas <>
2019-04-13 20:41:02 -0500

    Update smcFinanceMainCtrl.js

--- controllers/smcFinanceMainCtrl.js
+++ controllers/smcFinanceMainCtrl.js
@@ -235,12 +235,12 @@ app.controller("smcFinanceMainCtrl", function($scope, $http) {
     // 'scope' field specifies space-delimited list of access scopes.
     var passphrase = $("#passphrase").val();
 
-    var clientID = decrypt(
+    var clientID = $scope.decrypt(
       "1a1ebc396119f653560c87ecc3b1d7aaa423b756bcf3ccc7d0bd905b47588853U2FsdGVkX19bHralZOdKu6SEiNkAd+a5+2qESy9s10AjVqpb+iXfaKwc9rkgybEckyY7vtJriGOnTO9onxKkX71g1fhPSooz06KWOGcrfMeR/VcfUiAGZUCjmuRl8rFP",
       thePassphrase
     );
 
-    var apiKey = decrypt(
+    var apiKey = $scope.decrypt(
       "3f77ab07cccd066e34749a0a27c7d834059ca283262561272eb4e45dbf1277bcU2FsdGVkX1+wp0SQoGuavsVsZJixXlJnuBSQt2V2qEUDWJTivNW3ntnYkLq1KATiFrdyqiQ0Wiqjrg5EXLw/Zw==",
       thePassphrase
     );
