f8e31490e864abc56aee5e49559a018094fa62a5
stmarys-jsoc-dallas <>
2019-04-27 14:25:14 -0500

    Update smcFinanceMainCtrl.js

--- controllers/smcFinanceMainCtrl.js
+++ controllers/smcFinanceMainCtrl.js
@@ -270,7 +270,7 @@ app.controller("smcFinanceMainCtrl", function($scope, $http) {
                 */
               //.pipe(dest);
 
-              /*
+              console.log("Attempt 1");
 
               var encryptedAccessToken =
                 "81c039f3b5de095d0c68e55f6111f07122297dbbc3cfaacbfc04c14177842701U2FsdGVkX1++8EWFYkIcYdlGYV+73F11EoKGvlfw2x4+n7hRVeoayHb5xs1Lm2ZA3GditFiKsS955hZiEO7J1g==";
@@ -280,18 +280,23 @@ app.controller("smcFinanceMainCtrl", function($scope, $http) {
               );
               alert(accessToken);
               var xhr = new XMLHttpRequest();
-              xhr.open("GET", file.webContentLink, true);
+              xhr.open(
+                "GET",
+                "https://www.googleapis.com/drive/v3/files/" + file.id,
+                true
+              );
               xhr.responseType = "blob";
               xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
               xhr.onload = function(e) {
                 if (this.status == 200) {
+                  console.log("Got Response");
                   var myBlob = this.response;
                   $scope.parseExcel(myBlob);
                   // myBlob is now the blob that the object URL pointed to.
                 }
               };
               xhr.send("alt=media");
-*/
+
               console.log("Trying second option");
 
               var accessToken2 = gapi.auth2
@@ -311,8 +316,7 @@ app.controller("smcFinanceMainCtrl", function($scope, $http) {
               xhr.onload = function() {
                 alert("Got response");
                 //base64ArrayBuffer from https://gist.github.com/jonleighton/958841
-                var base64 =
-                  "data:image/png;base64," + base64ArrayBuffer(xhr.response);
+                var base64 = $scope.base64ArrayBuffer(xhr.response);
 
                 //do something with the base64 image here
               };
@@ -450,5 +454,58 @@ app.controller("smcFinanceMainCtrl", function($scope, $http) {
     } catch (error) {
       console.error(err);
     }
+  };
+
+  $scope.base64ArrayBuffer = function(arrayBuffer) {
+    var base64 = "";
+    var encodings =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    var bytes = new Uint8Array(arrayBuffer);
+    var byteLength = bytes.byteLength;
+    var byteRemainder = byteLength % 3;
+    var mainLength = byteLength - byteRemainder;
+
+    var a, b, c, d;
+    var chunk;
+
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3) {
+      // Combine the three bytes into a single integer
+      chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+
+      // Use bitmasks to extract 6-bit segments from the triplet
+      a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
+      b = (chunk & 258048) >> 12; // 258048   = (2^6 - 1) << 12
+      c = (chunk & 4032) >> 6; // 4032     = (2^6 - 1) << 6
+      d = chunk & 63; // 63       = 2^6 - 1
+
+      // Convert the raw binary segments to the appropriate ASCII encoding
+      base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
+    }
+
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+      chunk = bytes[mainLength];
+
+      a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
+
+      // Set the 4 least significant bits to zero
+      b = (chunk & 3) << 4; // 3   = 2^2 - 1
+
+      base64 += encodings[a] + encodings[b] + "==";
+    } else if (byteRemainder == 2) {
+      chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+
+      a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
+      b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
+
+      // Set the 2 least significant bits to zero
+      c = (chunk & 15) << 2; // 15    = 2^4 - 1
+
+      base64 += encodings[a] + encodings[b] + encodings[c] + "=";
+    }
+
+    return base64;
   };
 });
