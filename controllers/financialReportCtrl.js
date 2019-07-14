bb787dc887b5541d33f65a3d74fddf496f89870f
stmarys-jsoc-dallas <>
2019-07-14 00:17:22 -0500

    financialReportCtrl

--- controllers/financialReportCtrl.js
+++ controllers/financialReportCtrl.js
@@ -1,3 +1,23 @@
 app.controller("financialReportCtrl", function($scope, $http) {
-  $scope.testVariableHello = "Hello variable from hello.html";
+  $scope.buildfinancialSummary = function() {
+    var creditReport = {};
+    var debitReport = {};
+    for (var iTxnIndex = 0; i < transactions.length; iTxnIndex++) {
+      if (transactions[iTxnIndex].type == "CREDIT") {
+        var reason = transactions[iTxnIndex].reason;
+        if (creditReport[reason] == undefined) {
+          creditReport[reason] = 0;
+        }
+        creditReport[reason] += transactions[iTxnIndex].amount;
+      } else {
+        var reason = transactions[iTxnIndex].reason;
+        if (debitReport[reason] == undefined) {
+          debitReport[reason] = 0;
+        }
+        debitReport[reason] += transactions[iTxnIndex].amount;
+      }
+    }
+    $scope.creditReport = creditReport;
+    $scope.debitReport = debitReport;
+  };
 });
